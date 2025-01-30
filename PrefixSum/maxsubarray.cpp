53d968cbd989f6f83200ab78dae44b43c77699b7
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2025-01-30 11:23:35 +0530

    Update maxsubarray.cpp

--- PrefixSum/maxsubarray.cpp
+++ PrefixSum/maxsubarray.cpp
@@ -1,3 +1,30 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int> vec(n);
+//     for(int i=0;i<n;i++){
+//         cin>>vec[i];
+//     }
+//     // prefixing
+//     for(int i=1;i<n;i++){
+//         vec[i]+=vec[i-1];
+//     }
+
+//     int maxval=INT_MIN;
+//     int minval = 0;
+//     for(int i=0;i<n;i++){
+//         maxval = max(maxval,vec[i]-minval);
+//         minval = min(vec[i],minval);
+//     }
+//     cout<<maxval;
+//     return 0;
+// }
+
+// another method
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,17 +35,14 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>vec[i];
     }
-    // prefixing
-    for(int i=1;i<n;i++){
-        vec[i]+=vec[i-1];
-    }
-
     int maxval=INT_MIN;
     int minval = 0;
+    int prefix=0;
     for(int i=0;i<n;i++){
-        maxval = max(maxval,vec[i]-minval);
-        minval = min(vec[i],minval);
+        prefix+=vec[i];
+        maxval = max(maxval,prefix-minval);
+        minval = min(minval,prefix);
     }
-    cout<<maxval;
+    cout<<maxval<<endl;
     return 0;
 }

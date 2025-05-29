8e89d84158d7552f455e0953b6cf76bc834274a1
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2025-05-29 22:49:30 +0530

    update

--- Maths/testing.cpp
+++ Maths/testing.cpp
@@ -64,30 +64,93 @@
 // prime factorization
 // uses the divisor pairs so do till sqrt(n)
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+//     map<int,int> mp;
+//     for(int i=2;i*i<=n;i++){
+//         int count=0;
+//         if(n%i==0){
+//             while(n%i==0){
+//                 count++;
+//                 n = n/i;
+//             }
+//             mp[i] =count;
+//         }   
+//     }
+//     if(n!=0){
+//         mp[n]++;
+//     }
+
+//     for(auto &i:mp){
+//         cout<<i.first<<" "<<i.second<<endl;
+//     }
+//     return 0;
+// }
+
+
+// sieve of erethnoses
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+//     vector<int> vec(n,1);
+//     for(int i=2;i*i<=n;i++){
+//         if(vec[i]==1){
+//             for(int j=i;j<=n;j+=i){
+//                 vec[j]=0;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+
+// binary exponentiation
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     int power,base;
+//     cin>>power>>base;
+//     int result=1;
+//     while(power!=0){
+//         if(power%2!=0){
+//             result*=base;
+//             power-=1;
+//         }
+//         else{
+//             base*=base;
+//             power/=2;
+//         }
+//     }
+//     cout<<result;
+//     return 0;
+// }
+
+// gcd calculation
+
 #include <bits/stdc++.h>
 using namespace std;
 
+enum Flags{
+	RED,
+	GREEN,
+	YELLOW
+};
+
 int main(){
-
-    int n;
-    cin>>n;
-    map<int,int> mp;
-    for(int i=2;i*i<=n;i++){
-        int count=0;
-        if(n%i==0){
-            while(n%i==0){
-                count++;
-                n = n/i;
-            }
-            mp[i] =count;
-        }   
-    }
-    if(n!=0){
-        mp[n]++;
-    }
-
-    for(auto &i:mp){
-        cout<<i.first<<" "<<i.second<<endl;
-    }
+    int col = RED;
+    cout<<col;
     return 0;
 }
---
+++ Revision/prefixing.cpp
@@ -0,0 +1,62 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     int n,q;
+//     cin>>n>>q;
+//     // inputting
+//     vector<int> vec(n);
+//     for(int i=0;i<n;i++){
+//         cin>>vec[i];
+//         cout<<vec[i]<<endl;
+//     }
+//     // prefixing
+//     for(int i=1;i<n;i++){
+//         vec[i]+=vec[i-1];
+//     }
+//     while(q--){
+//         cout<<"inside this"<<endl;
+//         int l,r;
+//         cin>>l>>r;
+//         int result = vec[r];
+//         cout<<result<<endl;
+//         if(l>0){
+//             result-=vec[l-1];
+//         }
+//         cout<<result;
+//     }
+//     return 0;
+// }
+
+// subsequences summing to 7
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    vector<int> vec(n);
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+
+    // prefixing
+    for(int i=1;i<n;i++){
+        vec[i]+=vec[i-1];
+    }
+
+    // hashing
+    unordered_map<int,int> mp;
+    mp[0] = 0;
+    int maxl=0;
+    for(int i=0;i<n;i++){
+        int rem = vec[i]%7;
+        if(mp.find(rem)!=mp.end()){
+            maxl = max(maxl,i-mp[rem]+1);
+        }
+        mp[rem]=i;
+    }
+    cout<<maxl;
+    return 0;
+}

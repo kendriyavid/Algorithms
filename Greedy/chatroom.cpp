84c5616912bb6e0724caf970b14c0474a4d9a6f6
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2025-06-16 10:35:36 +0530

    Update chatroom.cpp

--- Greedy/chatroom.cpp
+++ Greedy/chatroom.cpp
@@ -1,31 +1,56 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+
+//     string s;
+//     cin>>s;
+//     string filtered="";
+//     char prev='-';
+//     for(char x:s){
+//         if(prev!=x){
+//             filtered+=x;
+//         }
+//         prev = x;
+//     }
+//     cout<<"filtered: "<<filtered<<endl;
+//     bool found=false;
+//     for(int i=0;i<filtered.length()-5+1;i++){
+//         if(filtered.substr(i,5)=="hello"){
+//             found=true;
+//             cout<<i<<endl;
+//         }
+//     }
+
+//     if(found){
+//         cout<<"yes"<<endl;
+//     }else{
+//         cout<<"no"<<endl;
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-
     string s;
-    cin>>s;
-    string filtered="";
-    char prev='-';
-    for(char x:s){
-        if(prev!=x){
-            filtered+=x;
+    cin >> s;
+    string hello = "hello";
+    int i = 0, j = 0;
+
+    while(i < hello.length() && j < s.length()){
+        if(hello[i] == s[j]){
+            i++;
         }
-        prev = x;
-    }
-    cout<<"filtered: "<<filtered<<endl;
-    bool found=false;
-    for(int i=0;i<filtered.length()-5+1;i++){
-        if(filtered.substr(i,5)=="hello"){
-            found=true;
-            cout<<i<<endl;
-        }
+        j++;
     }
 
-    if(found){
-        cout<<"yes"<<endl;
-    }else{
-        cout<<"no"<<endl;
+    if(i == hello.length()){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
     }
 
     return 0;
